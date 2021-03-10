46beac86e65d7994053bff34a0f0363c8963ecfb
mr-riz <rahulsharmaofficial356@gmail.com>
2021-03-10 21:56:43 +0530

    Sign_activity

--- app/src/main/java/com/example/fiber/SignInActivity.java
+++ app/src/main/java/com/example/fiber/SignInActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_sign_in);
     }
 }
