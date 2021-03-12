a22d15c0a345a05f175f56c9e4f1faeaebdcbb58
mr-riz <rahulsharmaofficial356@gmail.com>
2021-03-12 21:01:56 +0530

    Sign_activity

--- app/src/main/java/com/example/fiber/SignUpActivity.java
+++ app/src/main/java/com/example/fiber/SignUpActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.fiber.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
      ActivitySignUpBinding binding;
+     private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
+        auth=FirebaseAuth.getInstance();
     }
 }
