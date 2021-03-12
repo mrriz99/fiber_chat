cc3831e3bf1b743ee3b1526f0e498173efb89b89
mr-riz <rahulsharmaofficial356@gmail.com>
2021-03-12 21:39:20 +0530

    User_activity

---
+++ app/src/main/java/com/example/fiber/Models/Users.java
@@ -0,0 +1,4 @@
+package com.example.fiber.Models;
+
+public class Users {
+}
--- app/src/main/java/com/example/fiber/SignUpActivity.java
+++ app/src/main/java/com/example/fiber/SignUpActivity.java
@@ -3,19 +3,30 @@ package com.example.fiber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.fiber.databinding.ActivitySignUpBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
      ActivitySignUpBinding binding;
      private FirebaseAuth auth;
+     FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
-        auth=FirebaseAuth.getInstance();
+        auth =FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString() ,binding.etPassword.getText().toString());
+            }
+        });
     }
 }
