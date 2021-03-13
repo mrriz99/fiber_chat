f6ce227ca486dcf11e01c200645bed86c9cc8c77
mr-riz <rahulsharmaofficial356@gmail.com>
2021-03-13 19:58:47 +0530

    User_activity

--- app/src/main/java/com/example/fiber/SignUpActivity.java
+++ app/src/main/java/com/example/fiber/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.fiber;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -19,6 +20,10 @@ public class SignUpActivity extends AppCompatActivity {
      ActivitySignUpBinding binding;
      private FirebaseAuth auth;
      FirebaseDatabase database;
+     ProgressDialog progressDialog;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,19 +32,27 @@ public class SignUpActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         auth =FirebaseAuth.getInstance();
         database=FirebaseDatabase.getInstance();
+        progressDialog=new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We're creating your account");
+
+
         binding.btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                progressDialog.show();
                 auth.createUserWithEmailAndPassword
                         (binding.etEmail.getText().toString() ,binding.etPassword.getText().toString()).
                          addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
                         
                         if(task.isSuccessful()){
                             Users user=new Users(binding.etUsername.getText().toString(),binding.etEmail.getText().toString(),
                                     binding.etPassword.getText().toString());
                             String id= task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(user);
 
                             Toast.makeText(SignUpActivity.this, "User created Successfully", Toast.LENGTH_SHORT).show();
                         }
